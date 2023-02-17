4f13b09a6d0ac9afeb8c534825201bde3843a0ec
Luca v. K <luca.v.kannen@gmail.com>
2023-02-17 18:27:57 +0100

    Added rebuild script for the database and workbench file

---
+++ backend/db/rebuild.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS `data`;
+DROP TABLE IF EXISTS `energy_data`;
+DROP TABLE IF EXISTS `weather_data`;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`data` (
+  `timestamp` DATE NOT NULL,
+  `energy_data_id` INT NOT NULL,
+  `weather_data_id` INT NOT NULL,
+  PRIMARY KEY (`timestamp`),
+  INDEX `fk_data_energy_data_idx` (`energy_data_id` ASC) VISIBLE,
+  INDEX `fk_data_weather_data1_idx` (`weather_data_id` ASC) VISIBLE,
+  CONSTRAINT `fk_data_energy_data`
+    FOREIGN KEY (`energy_data_id`)
+    REFERENCES `mydb`.`energy_data` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_data_weather_data1`
+    FOREIGN KEY (`weather_data_id`)
+    REFERENCES `mydb`.`weather_data` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS `mydb`.`energy_data` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `battery_level` FLOAT NOT NULL,
+  `solar_panel_voltage` FLOAT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `mydb`.`weather_data` (
+  `id` INT NOT NULL,
+  `temperature` FLOAT NOT NULL,
+  `humidity` FLOAT NOT NULL,
+  `pressure` FLOAT NOT NULL,
+  `obstacle_detected` TINYINT NOT NULL,
+  `light_intensity` FLOAT NULL,
+  PRIMARY KEY (`id`, `obstacle_detected`)
+);
+
+ENGINE = InnoDB;
