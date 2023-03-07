5377180fa26cf7d651bdaa4eca1caa4b664034ac
Luca v. K <luca.v.kannen@gmail.com>
2023-03-07 13:02:26 +0100

    Implemented all the CRUD operations in a Rest API

--- docker-container/web/db.php
+++ docker-container/web/db.php
@@ -43,28 +43,80 @@ class Database {
     }
 
     public function deleteWeatherData($id) {
-        $sql = "DELETE FROM weather_data WHERE id='$id'";
+        $sql = "SELECT * FROM weather_data WHERE timestamp = '$id'";
+        $result = $this->conn->query($sql);
+
+        if (!$result) {
+            die("Error checking weather data: " . $this->conn->error);
+        }
+
+        if ($result->num_rows == 0) {
+            return false;
+        }
+
+        $sql = "DELETE FROM weather_data WHERE timestamp ='$id'";
+        $result = $this->conn->query($sql);
+
+        if (!$result) {
+            die("Error deleting weather data: " . $this->conn->error);
+        }
+
+        return true;
+    }
+
+    public function addWeatherData($timestamp, $temperature, $humidity, $pressure, $obstacle_detected, $light_intensity) {
+        // Check if the record already exists
+        $check_sql = "SELECT COUNT(*) AS count FROM weather_data WHERE timestamp = '$timestamp'";
+        $check_result = $this->conn->query($check_sql);
+        if (!$check_result) {
+            die("Error checking weather data: " . $this->conn->error);
+        }
+        $count = $check_result->fetch_assoc()['count'];
+        if ($count > 0) {
+            // Record already exists, do not add
+            return false;
+        }
+
+        // Record does not exist, add it
+        $sql = "INSERT INTO weather_data (timestamp, temperature, humidity, pressure, obstacle_detected, light_intensity) VALUES ('$timestamp', '$temperature', '$humidity', '$pressure', '$obstacle_detected', '$light_intensity')";
+        $result = $this->conn->query($sql);
+        if (!$result) {
+            die("Error adding weather data: " . $this->conn->error);
+        }
+        return true;
+    }
+
+    public function updateWeatherData($timestamp, $new_timestamp, $temperature, $humidity, $pressure, $obstacle_detected, $light_intensity) {
+        // Check if the record exists
+        $check_sql = "SELECT COUNT(*) AS count FROM weather_data WHERE timestamp = '$timestamp'";
+        $check_result = $this->conn->query($check_sql);
+        if (!$check_result) {
+            die("Error checking weather data: " . $this->conn->error);
+        }
+        $count = $check_result->fetch_assoc()['count'];
+        if ($count == 0) {
+            // Record does not exist, return false
+            return false;
+        }
+
+        // Record exists, update it
+        $sql = "UPDATE weather_data SET timestamp='$new_timestamp', temperature='$temperature', humidity='$humidity', pressure='$pressure', obstacle_detected='$obstacle_detected', light_intensity='$light_intensity' WHERE timestamp='$timestamp'";
+        $result = $this->conn->query($sql);
+        if (!$result) {
+            die("Error updating weather data: " . $this->conn->error);
+        }
+        return true;
+    }
+
+    public function deleteAllWeatherData() {
+        $sql = "DELETE FROM weather_data";
         $result = $this->conn->query($sql);
         if (!$result) {
             die("Error deleting weather data: " . $this->conn->error);
         }
     }
 
-    public function addWeatherData($timestamp, $temperature, $humidity, $pressure, $obstacle_detected, $light_intensity) {
-        $sql = "INSERT INTO weather_data (timestamp, temperature, humidity, pressure, obstacle_detected, light_intensity) VALUES ('$timestamp', '$temperature', '$humidity', '$pressure', '$obstacle_detected', '$light_intensity')";
-        $result = $this->conn->query($sql);
-        if (!$result) {
-            die("Error adding weather data: " . $this->conn->error);
-        }
-    }
 
-    public function updateWeatherData($id, $timestamp, $temperature, $humidity, $pressure, $obstacle_detected, $light_intensity) {
-        $sql = "UPDATE weather_data SET timestamp='$timestamp', temperature='$temperature', humidity='$humidity', pressure='$pressure', obstacle_detected='$obstacle_detected', light_intensity='$light_intensity' WHERE id='$id'";
-        $result = $this->conn->query($sql);
-        if (!$result) {
-            die("Error updating weather data: " . $this->conn->error);
-        }
-    }
 
 
 
--- docker-container/web/index.php
+++ docker-container/web/index.php
@@ -1,13 +1,14 @@
 <?php
 include 'db.php';
 
-
-
 //set up the database connection
 $db = new Database("mariadb", "root", "7YKyE8R2AhKzswfN", "WS");
 
+//check if the request method is set
 if (isset($_SERVER['REQUEST_METHOD'])) {
-    if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+
+    //Post operation to add data from the wemos
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Get the request body and decode it as JSON
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
@@ -29,7 +30,14 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
         $light_intensity = $data['light_intensity'];
 
         // Add the new datapoint to the weather_data table
-        $db->addWeatherData($timestamp, $temperature, $humidity, $pressure, $obstacle_detected, $light_intensity);
+        $success = $db->addWeatherData($timestamp, $temperature, $humidity, $pressure, $obstacle_detected, $light_intensity);
+
+        if (!$success) {
+            $response = array('error' => 'Error adding weather data.');
+            http_response_code(400);
+            echo json_encode($response);
+            die();
+        }
 
         // Return a success message
         $response = array('message' => 'Weather data added successfully.');
@@ -37,27 +45,102 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
         die();
     }
 
+    //Put operation for updating weather data
+    else if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+        // Get the request body and decode it as JSON
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+
+        // Check if the JSON decoding failed
+        if ($data === null) {
+            $response = array('error' => 'Invalid JSON format.');
+            http_response_code(400);
+            echo json_encode($response);
+            die();
+        }
+
+        // Extract the values from the JSON data
+        $timestamp = $data['timestamp'];
+        $new_timestamp = $data['new_timestamp'];
+
+        $temperature = $data['temperature'];
+        $humidity = $data['humidity'];
+        $pressure = $data['pressure'];
+        $obstacle_detected = $data['obstacle_detected'];
+        $light_intensity = $data['light_intensity'];
 
 
-    if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == '/') {
-        // Return the index.html file
+        // Update the existing datapoint in the weather_data table
+        $success = $db->updateWeatherData($timestamp,$new_timestamp, $temperature, $humidity, $pressure, $obstacle_detected, $light_intensity);
+
+        if (!$success) {
+            $response = array('error' => 'Error updating weather data.');
+            http_response_code(400);
+            echo json_encode($response);
+            die();
+        }
+
+        // Return a success message
+        $response = array('message' => 'Weather data updated successfully.');
+        echo json_encode($response);
+        die();
+    }
+
+    //Delete operation
+    else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    // Get the request body and decode it as JSON
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+
+    // Check if the JSON decoding failed
+    if ($data === null) {
+        $response = array('error' => 'Invalid JSON format.');
+        http_response_code(400);
+        echo json_encode($response);
+        die();
+    }
+
+    // Extract the values from the JSON data
+    $timestamp = $data['timestamp'];
+
+    // Delete the datapoint from
+        // Delete the datapoint from the weather_data table
+        $success = $db->deleteWeatherData($timestamp);
+
+        if(!$success) {
+            $response = array('error' => 'Error deleting weather data.');
+            http_response_code(400);
+            echo json_encode($response);
+            die();
+        }
+
+        // Return a success message
+        $response = array('message' => 'Weather data deleted successfully.');
+        echo json_encode($response);
+        die();
+    }
+    // Serve the main page
+    else if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == '/') {
+        //Serves the main page
         header('Content-type: text/html');
         readfile('index.html');
         die();
     }
-
+    //Get operation for the weather data for the frontend
     else if ($_GET['action'] == 'weather_data') {
+        // Return the weather data
         $result = $db->getWeatherData();
         echo $result;
         die();
     }
 }
+//Error handling
 else {
+    //When the backend is not on a server
     echo "Error: This script is not being executed as part of an HTTP request.";
     die();
 }
-
-
-
+//Close the database connection
 $db->close();
 ?>
+
