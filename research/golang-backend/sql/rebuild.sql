d41ee7eb5e04137ed3989c8a95a63576d495ca3f
Luca v. K <luca.v.kannen@gmail.com>
2023-03-23 17:29:51 +0100

    Updated rebuild script

--- research/golang-backend/sql/rebuild.sql
+++ research/golang-backend/sql/rebuild.sql
@@ -1,19 +1,21 @@
-DROP TABLE IF EXISTS `WS`.`energy_data`;
-DROP TABLE IF EXISTS `WS`.`weather_data`;
+CREATE SCHEMA IF NOT EXISTS ws;
 
-CREATE TABLE IF NOT EXISTS `ws`.`energy_data`(
-                                                 `timestamp` DATE NOT NULL,
-                                                 `battery_level` FLOAT NOT NULL,
-                                                 `solar_panel_voltage` FLOAT NOT NULL,
-                                                 PRIMARY KEY(`timestamp`)
+DROP TABLE IF EXISTS "WS"."weather_data";
+DROP TABLE IF EXISTS "WS"."energy_data";
+
+CREATE TABLE IF NOT EXISTS "ws"."energy_data"(
+                                                 "timestamp" DATE NOT NULL,
+                                                 "battery_level" FLOAT NOT NULL,
+                                                 "solar_panel_voltage" FLOAT NOT NULL,
+                                                 PRIMARY KEY("timestamp")
     );
 
-CREATE TABLE IF NOT EXISTS `ws`.`weather_data`(
-                                                  `timestamp` DATE NOT NULL,
-                                                  `temperature` FLOAT NOT NULL,
-                                                  `humidity` FLOAT NOT NULL,
-                                                  `pressure` FLOAT NOT NULL,
-                                                  `obstacle_detected` BOOLEAN NOT NULL,
-                                                  `light_intensity` FLOAT NOT NULL,
-                                                  PRIMARY KEY(`timestamp`)
+CREATE TABLE IF NOT EXISTS "ws"."weather_data"(
+                                                  "timestamp" DATE NOT NULL,
+                                                  "temperature" FLOAT NOT NULL,
+                                                  "humidity" FLOAT NOT NULL,
+                                                  "pressure" FLOAT NOT NULL,
+                                                  "obstacle_detected" BOOLEAN NOT NULL,
+                                                  "light_intensity" FLOAT NOT NULL,
+                                                  PRIMARY KEY("timestamp")
     );
