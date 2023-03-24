0840cfcbdcd925b7071d7fb2b3b56c1a7152336f
Luca v. K <luca.v.kannen@gmail.com>
2023-03-24 13:44:10 +0100

    Reformated code structure and commented code in db manager

--- research/golang-backend/db.go
+++ research/golang-backend/db.go
@@ -10,7 +10,46 @@ import (
 	"path/filepath"
 )
 
-func setup_db(d *dbManager) {
+//dbManager struct
+
+type dbManager struct {
+	db         *gorm.DB
+	name       string
+	insertSql  string
+	rebuildSql string
+}
+
+// NewDBManager constructor for dbManager
+func NewDBManager(db *gorm.DB, name string) *dbManager {
+	return &dbManager{db: db, name: name, insertSql: "insert.sql", rebuildSql: "rebuild.sql"}
+}
+
+//basic functions
+
+func (d *dbManager) SetDBManager(db *gorm.DB, name string) {
+	d.SetName(name)
+	d.SetDB(db)
+}
+
+func (d *dbManager) SetDB(db *gorm.DB) {
+	d.db = db
+}
+
+func (d *dbManager) SetName(name string) {
+	d.name = name
+}
+
+func (d *dbManager) GetDB() *gorm.DB {
+	return d.db
+}
+
+func (d *dbManager) GetName() string {
+	return d.name
+}
+
+//setup function
+
+func (d *dbManager) setupDb() {
 	dsn := "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable"
 	var err error
 	d.db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -34,26 +73,22 @@ func setup_db(d *dbManager) {
 	//show all tables content
 
 }
-func (d *dbManager) runSqlSetupFiles() {
-	// Check if SQL file exists
-	sqlFilePath := filepath.Join(".", "sql", d.rebuildSql)
-	if _, err := os.Stat(sqlFilePath); os.IsNotExist(err) {
-		panic(fmt.Sprintf("SQL file '%s' not found", sqlFilePath))
-	}
 
-	// Read SQL file
-	sqlBytes, err := ioutil.ReadFile(sqlFilePath)
+//Additional functions
+
+func (d *dbManager) logAllTables() {
+	rows, err := d.db.Raw("SELECT table_name FROM information_schema.tables WHERE table_schema='ws'").Rows()
 	if err != nil {
-		panic(err)
+		log.Default().Println(err)
 	}
-	sql := string(sqlBytes)
-
-	// Execute SQL
-	if err := d.db.Exec(sql).Error; err != nil {
-		panic(err)
+	log.Default().Println("Tables: ")
+	for rows.Next() {
+		var table_name string
+		rows.Scan(&table_name)
+		log.Default().Println(table_name)
 	}
-	log.Default().Println("Executed ", d.rebuildSql)
-
+	//line break
+	log.Default().Println()
 }
 
 func (d *dbManager) fillDB() {
@@ -83,50 +118,24 @@ func (d *dbManager) fillDB() {
 	log.Default().Println("Executed ", d.insertSql)
 }
 
-type dbManager struct {
-	db         *gorm.DB
-	name       string
-	insertSql  string
-	rebuildSql string
-}
+func (d *dbManager) runSqlSetupFiles() {
+	// Check if SQL file exists
+	sqlFilePath := filepath.Join(".", "sql", d.rebuildSql)
+	if _, err := os.Stat(sqlFilePath); os.IsNotExist(err) {
+		panic(fmt.Sprintf("SQL file '%s' not found", sqlFilePath))
+	}
 
-func (d *dbManager) SetDBManager(db *gorm.DB, name string) {
-	d.SetName(name)
-	d.SetDB(db)
-}
-
-func (d *dbManager) SetDB(db *gorm.DB) {
-	d.db = db
-}
-
-func (d *dbManager) SetName(name string) {
-	d.name = name
-}
-
-func (d *dbManager) GetDB() *gorm.DB {
-	return d.db
-}
-
-func (d *dbManager) GetName() string {
-	return d.name
-}
-
-func (d *dbManager) logAllTables() {
-	rows, err := d.db.Raw("SELECT table_name FROM information_schema.tables WHERE table_schema='ws'").Rows()
+	// Read SQL file
+	sqlBytes, err := ioutil.ReadFile(sqlFilePath)
 	if err != nil {
-		log.Default().Println(err)
+		panic(err)
 	}
-	log.Default().Println("Tables: ")
-	for rows.Next() {
-		var table_name string
-		rows.Scan(&table_name)
-		log.Default().Println(table_name)
-	}
-	//line break
-	log.Default().Println()
-}
+	sql := string(sqlBytes)
 
-// NewDBManager constructor for dbManager
-func NewDBManager(db *gorm.DB, name string) *dbManager {
-	return &dbManager{db: db, name: name, insertSql: "insert.sql", rebuildSql: "rebuild.sql"}
+	// Execute SQL
+	if err := d.db.Exec(sql).Error; err != nil {
+		panic(err)
+	}
+	log.Default().Println("Executed ", d.rebuildSql)
+
 }
