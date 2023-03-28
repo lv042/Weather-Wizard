df5cbb314d06b78c1824f82878b0bd84afebb53e
Luca v. K <luca.v.kannen@gmail.com>
2023-03-28 20:34:33 +0200

    Setup get and post requests

--- research/golang-backend/main.go
+++ research/golang-backend/main.go
@@ -40,5 +40,46 @@ func (f *fiberApp) InitFiber() {
 }
 
 func (f *fiberApp) setupRoutes() {
+	// Get all weather data
+	f.fiberApp.Get("/api/weather_data", func(c *fiber.Ctx) error {
+		data, err := dbManager.GetAllWeatherDataJSON()
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+		return c.Send(data)
+	})
 
+	// Get weather data by timestamp
+	f.fiberApp.Post("/api/weather_data/timestamp", func(c *fiber.Ctx) error {
+		data := new(struct {
+			Timestamp string `json:"timestamp"`
+		})
+		if err := c.BodyParser(data); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": "Invalid request body",
+			})
+		}
+		result, err := dbManager.GetWeatherDataByTimestampJSON(string(c.Body()))
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+		return c.SendString(result)
+	})
+
+	// Create weather data
+	f.fiberApp.Post("/api/weather_data", func(c *fiber.Ctx) error {
+		result, err := dbManager.CreateWeatherDataJSON(string(c.Body()))
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+		return c.JSON(fiber.Map{
+			"message": result,
+		})
+	})
 }
