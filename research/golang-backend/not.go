f3d8fbf0765f4828eca5564a2b5ef3cebb5cf78c
Luca v. K <luca.v.kannen@gmail.com>
2023-04-03 14:45:17 +0200

    API key gets now read from file

--- research/golang-backend/not.go
+++ research/golang-backend/not.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	"os"
 )
 
 var email string
 var enabled bool
+var key string
 
 func setupEmail(e string, en bool) {
 	email = e
 	enabled = en
-	message := "This is a test message"
-	sendMail(email, enabled, message)
+	readKey()
+	sendMail(email)
 }
 
-func sendMail(email string, enabled bool, message string) {
+func readKey() {
+	file, err := os.Open("sendgrid.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		key = scanner.Text()
+	}
+}
+
+func sendMail(email string) {
 	from := mail.NewEmail("Luca", "luca.v.kannen@gmail.com")
 	subject := "Backend Notifications"
 	to := mail.NewEmail("Luca", email)
@@ -24,7 +40,7 @@ func sendMail(email string, enabled bool, message string) {
 		"this email address. You will receive a daily summary of all requests and errors. You can unsubscribe in the Admin Panel."
 	htmlContent := "<p>Hi there, you are now subscribed to notifications.</p>\n\n<p>The backend will send from now on notifications to this email address. You will receive a daily summary of all requests and errors.</p>\n\n<p>You can unsubscribe in the Admin Panel."
 	emailMessage := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient("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")
+	client := sendgrid.NewSendClient(key)
 	response, err := client.Send(emailMessage)
 	if err != nil {
 		fiberApp.Log(fmt.Sprintf("Error: %s", err))
